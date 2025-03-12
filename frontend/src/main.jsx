ad52697c66953cb42b71aadf14263c8ee34d1579
unknown <shaikameen1152001@gmail.com>
2025-03-13 00:08:52 +0530

    first commit

---
+++ frontend/src/main.jsx
@@ -0,0 +1,12 @@
+
+// import { createRoot } from 'react-dom/client'
+import React from 'react'
+import ReactDom from 'react-dom/client'
+import App from './App.jsx'
+
+ReactDom.createRoot(document.getElementById('root')).render(
+  <React.StrictMode><App /></React.StrictMode>
+
+    
+  
+)
---
+++ frontend/src/pages/NotFound.jsx
@@ -0,0 +1,10 @@
+function NotFound() {
+    return (
+      <div>
+        <h1> 404 NotFound</h1>
+        <h2> the page you are looking </h2>
+      </div>
+    );
+  }
+  
+  export default NotFound;
